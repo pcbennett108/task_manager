b3e99bface26734968e23c3e7e226702071a980b
pcbennett108 <pcbennett42@gmail.com>
2023-08-16 20:28:21 -0600

    Add new task comtroller, view and route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,16 @@ class TasksController < ApplicationController
   def index
     @tasks = ["Task 1", "Task 2", "Task 3"]
   end
+
+  def new
+  end
+
+  def create
+    task = Task.new({
+      title: params[:title],
+      description: params[:description]
+    })
+    task.save
+    redirect_to "/tasks"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/", to: "welcome#index"
-  
+
   get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new"
+  post "/tasks", to: "tasks#create"
 end
